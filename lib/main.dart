bcd8a9d377f4ec6774c13d6c6808d9d8906f642c
Jon Gomez <jg3425@nau.edugit config --global user.email jg3425@nau.edugit config --global user.email jg3425@nau.edu>
2021-03-19 14:09:38 -0700

    Added date and time

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'HealthyRecipes.dart';
@@ -18,7 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primaryColor: Colors.lightBlueAccent,
         // This is the theme of your application.
         //
@@ -59,130 +60,192 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage>
-with SingleTickerProviderStateMixin{
-
+    with SingleTickerProviderStateMixin {
   TabController tabController;
+
+  Timer clockUpdateTimer;
+  DateTime time = DateTime.now();
+  DateTime currentDate;
+  String date;
+  String hour, min;
 
   @override
   void initState() {
-
     super.initState();
     tabController = new TabController(length: 4, vsync: this);
+    currentDate = DateTime(time.year, time.month, time.day);
+    date = '${getMonth(currentDate)} , ${time.day}';
+    hour = '${time.hour}';
+    min = reformatMinute(time);
+    clockUpdateTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+      updateTimer();
+    });
     //TODO: Implement landing page
+  }
+
+  /// Updates the current clock time
+  void updateTimer() {
+    time = DateTime.now();
+    setState(() {
+      currentDate = DateTime(time.year, time.month, time.day);
+      date ='${getMonth(time)}, ${time.day}';
+      hour = '${time.hour}';
+      min = reformatMinute(time);
+    });
+  }
+
+  /// Returns a string with the correct month name using input date
+  String getMonth(DateTime date) {
+    switch (date.month)
+    {
+      case 1: return 'January'; break;
+      case 2: return 'February'; break;
+      case 3: return 'March'; break;
+      case 4: return 'April'; break;
+      case 5: return 'May'; break;
+      case 6: return 'June'; break;
+      case 7: return 'July'; break;
+      case 8: return 'August'; break;
+      case 9: return 'September'; break;
+      case 10: return 'October'; break;
+      case 11: return 'November'; break;
+      case 12: return 'December'; break;
+      default: throw Exception('Date month is not 1-12'); break;
+    }
+  }
+
+  /// Returns a string with a 0 infront if a single digit minute, for use with the displayed minutes on home page
+  String reformatMinute(DateTime date) {
+    if (date.minute < 10)
+      return '0${date.minute}';
+    else
+      return '${date.minute}';
   }
 
   @override
   // builds the view
   Widget build(BuildContext context) {
-   // scaffold generates the UI
-   return new Scaffold(
-     // App bar is the top most bar of the app
-     appBar: AppBar(
-       title: Text('Olakino'),
-     ),
-    // This is the menu drawer
-     drawer: Drawer(
-       // ListTitle is inherited by ListTitle
-       child: ListView(
-         //
-         padding: EdgeInsets.zero,
-         children: <Widget>[
-           DrawerHeader(
-             child: Text('Olakino', style: TextStyle(
-               fontSize: 20
+    // scaffold generates the UI
+    return new Scaffold(
+      // App bar is the top most bar of the app
+      appBar: AppBar(
+        title: Text('Olakino'),
+      ),
 
-             ),),
-             decoration: BoxDecoration(
-               image: DecorationImage(
-                 image: NetworkImage('https://i.ytimg.com/vi/bKrym5zuOag/maxresdefault.jpg'),
-                 fit: BoxFit.contain
-               )
-             ),
-           ),
+      body: Column(
+          children: [
+            Center (
+              child: Text('$date',
+                style: TextStyle(fontSize: 40)),
+            ),
+            Center (
+              child: Text('$hour:$min',
+                style: TextStyle(fontSize: 40)),
+            ),
+          ],
+      ),
 
-           /*
+      // This is the menu drawer
+      drawer: Drawer(
+          // ListTitle is inherited by ListTitle
+          child: ListView(
+        //
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Olakino',
+              style: TextStyle(fontSize: 20),
+            ),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://i.ytimg.com/vi/bKrym5zuOag/maxresdefault.jpg'),
+                    fit: BoxFit.contain)),
+          ),
+
+          /*
               Profile ListTile
             */
-           ListTile(
-               leading: Text('Home'),
-               trailing: Icon(Icons.home),
-               /*
+          ListTile(
+              leading: Text('Home'),
+              trailing: Icon(Icons.home),
+              /*
                  This function handles linking the button to the class which contains the
                  page
                 */
-               onTap: () {
-                 // this clears the page so it can be changed to another
-                 Navigator.pop(context);
-                 // this pushes the content of a particular page, in this case the Profile class
-                 Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
-               }
-           ),
-
-           ListTile(
-             leading: Text('User Profile'),
-             trailing: Icon(Icons.account_circle),
-               /*
-                 This function handles linking the button to the class which contains the
-                 page
-                */
-               onTap: () {
-                 // this clears the page so it can be changed to another
-                 Navigator.pop(context);
-                 // this pushes the content of a particular page, in this case the Profile class
-                 Navigator.push(context, MaterialPageRoute(builder: (context) => Profile()));
-             }
-           ),
-           // recipes
-           ListTile(
-             leading: Text('Healthy Recipes'),
-             trailing: Icon(Icons.fastfood),
-
-             /*
-               This function handles linking the button to the class which contains
-               the page
-              */
               onTap: () {
+                // this clears the page so it can be changed to another
                 Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => HealthyRecipes()));
+                // this pushes the content of a particular page, in this case the Profile class
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => MyApp()));
+              }),
 
+          ListTile(
+              leading: Text('User Profile'),
+              trailing: Icon(Icons.account_circle),
+              /*
+                 This function handles linking the button to the class which contains the
+                 page
+                */
+              onTap: () {
+                // this clears the page so it can be changed to another
+                Navigator.pop(context);
+                // this pushes the content of a particular page, in this case the Profile class
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Profile()));
+              }),
+          // recipes
+          ListTile(
+            leading: Text('Healthy Recipes'),
+            trailing: Icon(Icons.fastfood),
 
-              },
-           ),
-
-           ListTile(
-             leading: Text('Exercises'),
-             trailing: Icon(Icons.fitness_center),
-             /*
+            /*
                This function handles linking the button to the class which contains
                the page
               */
-             onTap: () {
-               Navigator.pop(context);
-               Navigator.push(context, MaterialPageRoute(builder: (context) => ExerciseList()));
-             },
-           ),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => HealthyRecipes()));
+            },
+          ),
 
-           // Game aspect
-           ListTile(
-             leading: Text('Exercise RPG'),
-             trailing: Icon(Icons.videogame_asset),
+          ListTile(
+            leading: Text('Exercises'),
+            trailing: Icon(Icons.fitness_center),
+            /*
+               This function handles linking the button to the class which contains
+               the page
+              */
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ExerciseList()));
+            },
+          ),
 
-             /*
+          // Game aspect
+          ListTile(
+              leading: Text('Exercise RPG'),
+              trailing: Icon(Icons.videogame_asset),
+
+              /*
                 This function handles linking the button to the class which
                  contains the page
               */
-             onTap: () {
-               Navigator.pop(context);
-               Navigator.push(context,MaterialPageRoute(builder: (context) => GameClass()));
-             }
-           )
-
-
-         ],
-
-       )
-     ),
-
-   );
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => GameClass()));
+              })
+        ],
+      )),
+    );
   }
 }
+
+
+
+
